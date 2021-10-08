c7fc0355bdea04fc32a01a242c48982b1cb30d1a
Robert Kesterson <robert.d.kesterson@leidos.com>
2021-10-08 17:48:39 -0400

    setup testing in all packages

--- packages/react-diagrams-defaults/jest.config.js
+++ packages/react-diagrams-defaults/jest.config.js
@@ -1,6 +1,7 @@
 const sharedConfig = require('../../jest.config.js')(__dirname);
 module.exports = {
 	...sharedConfig,
+	testEnvironment: 'jsdom',
 	collectCoverageFrom: [
 		...sharedConfig.collectCoverageFrom,
 		'!src/index.ts'
